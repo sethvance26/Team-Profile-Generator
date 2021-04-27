232cf2d09665db998e5bb2ac3438f82d6bbe7ccb
Seth Vance <sethvance26@gmail.com>
2021-04-27 13:08:04 -0400

    Writing tests for Intern.js

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -4,4 +4,16 @@ test('Can set school name via constructor', () => {
     const testValue = "SchoolName";
     const e = new Intern("Foo", 1, "test testing", testValue);
     expect (e.school).toBe(testValue);
-});
+});  
+
+test("getRole() should return \"Intern\"", () => {
+    const testValue = "Intern";
+    const e = new Intern("Foo", 1, "test testing", "SchoolName");
+    expect(e.getRole()).toBe(testValue);
+  });
+
+  test("Can get School name via getSchool()", () => {
+    const testValue = "SchoolName";
+    const e = new Engineer("Foo", 1, "test testing", testValue);
+    expect(e.getSchool()).toBe(testValue);
+  });
