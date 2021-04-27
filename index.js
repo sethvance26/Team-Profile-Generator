464177505202c2496a94ed00171e1e46f689fe83
Seth Vance <sethvance26@gmail.com>
2021-04-27 13:51:23 -0400

    Writing base questions, trying to prompt user

--- index.js
+++ index.js
@@ -5,8 +5,56 @@ const fs = require('fs');
 const jest = require('jest');
 //Importing our classes for each role
 
-const Manager = require('./lib/Manager.js');
-const Employee = require('./lib/Employee.js');
-const Intern = require('./lib/Intern');
-const Engineer = require('./lib/Engineer');
+const roles = require('../Team-Profile-Generator/lib');
+
+const questions = [
+    {
+        type: 'list',
+        name: 'type',
+        message: 'Which role would you like to build?',
+        choices: ['Manager', 'Engineer', 'Intern'],
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of this employee?',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is their employee ID?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is their e-mail address?',
+    },
+
+]
+    // const fileWrite = () => {
+    //     inquirer
+    //         .prompt(questions)
+    //         .then((data) => {
+    //             console.log("data", data)
+    //             fs.writeFileSync('newteam.html', objectMap.renderHtml(data.)
+    //         }
+    // }
+
+    const userPrompt = () => {
+        inquirer.prompt(questions)
+        fs.writeFile('newTeam.html', data, (error) =>
+        error ? console.log('Error!') : console.log('Successful!'));
+      }
+      
+      
+      //This function initializes our application and tells the userPrompt function to run first, then takes that data generates a markdown, and writes the README with that information. 
+      const init = () => {
+      userPrompt()
+      };
+    
+      
+      // Function call to initialize app
+      init();
+
+
 
