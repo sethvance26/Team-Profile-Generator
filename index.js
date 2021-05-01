7f4a19fa970974f7c2b9d6d3eb0c39528184fa89
Seth <sethvance26@gmail.com>
2021-04-30 20:27:14 -0400

    Making it look *Prettier*

--- index.js
+++ index.js
@@ -1,93 +1,128 @@
-
 //Here we're importing inquirer, fs, and jest
-const inquirer = require('inquirer');
-const fs = require('fs');
-const jest = require('jest');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const jest = require("jest");
 
 //Importing our classes for each role
 
-const Employee = require('../Team-Profile-Generator/lib/Employee');
-const Intern = require('../Team-Profile-Generator/lib/Intern');
-const Engineer = require('../Team-Profile-Generator/lib/Engineer');
-const Manager = require('../Team-Profile-Generator/lib/Manager');
-
-
+const Employee = require("../Team-Profile-Generator/lib/Employee");
+const Intern = require("../Team-Profile-Generator/lib/Intern");
+const Engineer = require("../Team-Profile-Generator/lib/Engineer");
+const Manager = require("../Team-Profile-Generator/lib/Manager");
 
 const teamMembers = [];
 
 const engineerQuestion = [
   {
-    type: 'input',
-    name: 'engineerGithub',
-    message:'What is your github username?',
-  }];
-  
-  const internQuestion = [
+    type: "input",
+    name: "engineerGithub",
+    message: "What is your github username?",
+  },
+];
+
+const internQuestion = [
   {
-    type: 'input',
-    name: 'internSchool',
-    message: 'Please enter intern school name',
-  }];
+    type: "input",
+    name: "internSchool",
+    message: "Please enter intern school name",
+  },
+];
 
-  const managerQuestion = [
-    {
-      type: 'input',
-      name: 'officeNum',
-      message: 'Enter Office Number for Manager',
-    }];
-
+const managerQuestion = [
+  {
+    type: "input",
+    name: "officeNum",
+    message: "Enter Office Number for Manager",
+  },
+];
 
 const questions = () => {
-return inquirer.prompt([
-    {
-        type: 'list',
-        name: 'role',
-        message: 'Which role would you like to build?',
-        choices: ['Manager', 'Engineer', 'Intern'],
-    },
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of this employee?',
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is their employee ID?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is their e-mail address?',
-    },
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "Which role would you like to build?",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of this employee?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is their employee ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is their e-mail address?",
+      },
+    ])
 
-])
+    .then((data) => {
+      if (data.role === "Manager") {
+        return inquirer
+          .prompt(managerQuestion)
 
+          .then((data2) => {
+            const ManagerGuy = new Manager(
+              data.name,
+              data.email,
+              data.id,
+              data2.officeNum
+            );
+            teamMembers.push(ManagerGuy);
+          });
+      }
+      if (data.role === "Manager") {
+        return inquirer
+          .prompt(managerQuestion)
 
-.then(data => {
-    if (data.role === 'Manager') {
-      return inquirer.prompt(managerQuestion)
-    
-.then(data2 => {
-  
-  const ManagerGuy = new Manager (data.name, data.email, data.id, data2.officeNum,  )
-  teamMembers.push(ManagerGuy);
+          .then((data2) => {
+            const ManagerGuy = new Manager(
+              data.name,
+              data.email,
+              data.id,
+              data2.officeNum
+            );
+            teamMembers.push(ManagerGuy);
+          });
+      }
+      if (data.role === "Manager") {
+        return inquirer
+          .prompt(managerQuestion)
 
-});
-    
-}})
+          .then((data2) => {
+            const ManagerGuy = new Manager(
+              data.name,
+              data.email,
+              data.id,
+              data2.officeNum
+            );
+            teamMembers.push(ManagerGuy);
+          });
+      }
+      if (data.role === "Manager") {
+        return inquirer
+          .prompt(managerQuestion)
 
-
+          .then((data2) => {
+            const ManagerGuy = new Manager(
+              data.name,
+              data.email,
+              data.id,
+              data2.officeNum
+            );
+            teamMembers.push(ManagerGuy);
+          });
+      }
+    });
 }; //This is where the questions functions ends.
 
-
-
-
-
-
-
-    
-const Markup = (data) => 
+const Markup = (data) =>
   `<!DOCTYPE html>
   <html lang="en">
   <head>
@@ -137,22 +172,21 @@ const Markup = (data) =>
   </body>
   </html>`;
 
-
-  
- // Function to write README file
+// Function to write README file
 const writeToFile = (data) => {
-  fs.writeFile('./dist/index.html', Markup(data), (error) =>
-  error ? console.log('Error!') : console.log('Success!'));
-}
-
+  fs.writeFile("./dist/index.html", Markup(data), (error) =>
+    error ? console.log("Error!") : console.log("Success!")
+  );
+};
 
 // Function to initialize app
 const init = () => {
   questions()
-    .then((data) => { 
-      console.log(teamMembers)
-      writeToFile('./dist/index.html', Markup(data))})  
-    .then(() => console.log('Successfully wrote an index.html'))
+    .then((data) => {
+      console.log(teamMembers);
+      writeToFile("./dist/index.html", Markup(data));
+    })
+    .then(() => console.log("Successfully wrote an index.html"))
     .catch((err) => console.error(err));
 };
 
